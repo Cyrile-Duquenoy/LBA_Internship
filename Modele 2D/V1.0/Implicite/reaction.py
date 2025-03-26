75af68fa1623be73030dd06a6fb097bb9ba14741
Cyrile-Duquenoy <cyrileduquenoy@hotmail.fr>
2025-03-26 11:17:58 +0100

    Delete Modele 2D/V1.0 directory

--- Modele 2D/V1.0/Implicite/reaction.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def reaction(C,F, alpha, beta, gamma, delta, F_max, C_max, dx):
-    Xsi=2
-    cytok_reac = 0
-    fib_reac = gamma*C*(F/(F + F_max))  + F*np.gradient(Xsi*np.gradient(C)) - delta*F
-    return cytok_reac, fib_reac
--- Modele 2D/V1.0/is_cfl.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def is_cfl(dt,dx,D):
-    return dt <= D*dx**2/4
-    
-
